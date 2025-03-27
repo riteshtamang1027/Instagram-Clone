bdf8d1df8ccc11fe0bd25a62f15bc97ac79e867b
Ritesh Tamang <riteshtmglama001@gmail.com>
2025-03-27 12:24:26 +0545

    add actual front end url

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,10 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173", 
+    origin: [
+      "http://localhost:5173",
+      "https://instagram-mocha-three.vercel.app/",
+    ],
   })
 );
 
